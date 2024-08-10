6fb1a9bf0b6f6c888360daac12355e8aa81f8a49
vikas-shirsath <vikasshirsath04@gmail.com>
2024-08-10 23:10:21 +0530

    practice

---
+++ LinkedList/LL.java
@@ -0,0 +1,16 @@
+public class LL {
+    class Node {
+        String data;
+        Node next;
+
+        Node (String data) {
+            this.data = data;
+            this.next = null;
+        } 
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+         
+    }
+}
