48c378e2b413c0c457e618faa34c9d5a605344ba
vikas-shirsath <vikasshirsath04@gmail.com>
2024-08-10 12:59:14 +0530

    Practice

---
+++ LeetCode/Easy/KthDistinctString.java
@@ -0,0 +1,23 @@
+package LeetCode.Easy;
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        String distinct = "";
+        int count = 0;
+        for (int i=0; i<arr.length; i++) {
+            boolean flag = true;
+            for(int j=0; j<arr.length; j++) {
+                if(arr[i].equals(arr[j]) && i!=j) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                count++;
+                if(count == k) {
+                    return arr[i];
+                }
+            }
+        }
+        return "";
+    }
+}
