8666e30763b9e0776372ae4ebaae7549ceea4312
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-09-11 18:52:13 +0530

    Practice

---
+++ LeetCode/Easy/171.java
@@ -0,0 +1,15 @@
+//171. Excel Sheet Column Number
+
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int count = 0;
+        int num = 0;
+        for(int i = columnTitle.length() -1; i>=0; i--) {
+            int current = columnTitle.charAt(i) - 'A' + 1;
+            current = current * (int)Math.pow(26, count);
+            num = num + current;
+            count ++;
+        }
+        return num;
+    }
+}
