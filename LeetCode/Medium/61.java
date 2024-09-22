9d9b088a1465781bd66e027aad504fa9133f38dd
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-09-22 19:30:51 +0530

    practice

---
+++ LeetCode/Medium/61.java
@@ -0,0 +1,36 @@
+//61. Rotate List
+//Given the head of a linked list, rotate the list to the right by k places.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || k == 0 || head.next == null) {
+            return head;
+        }
+        ListNode temp = head;
+        int length = 1;
+        while (temp.next != null) {
+            temp = temp.next;
+            length++;
+        }
+        temp.next = head;
+        k = k % length;
+        int stepsToNewTail = length - k;
+        ListNode newTail = head;
+        for (int i = 1; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+        return newHead;
+    }
+}
