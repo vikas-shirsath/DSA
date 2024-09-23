1787933da88571942bbb8d2f0d157bd0c32e4545
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-09-24 01:06:23 +0530

    Practice

---
+++ LeetCode/Easy/326.java
@@ -0,0 +1,15 @@
+//326. Power of Three
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0) {
+            return false;
+        }
+
+        while(n % 3 == 0) {
+            n = n / 3;
+        }
+
+        return n == 1;
+    }
+}
