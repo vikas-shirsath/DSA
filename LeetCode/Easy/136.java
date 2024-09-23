99db58d4903732e3239e0b2cc5200ee861a1aa0d
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-09-23 22:53:11 +0530

    Practice

---
+++ LeetCode/Easy/136.java
@@ -0,0 +1,11 @@
+//136. Single Number
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
