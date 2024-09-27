e59d47488329193a9c489d2ec6f83064000f0ce9
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-09-28 00:09:23 +0530

    Practice

---
+++ LeetCode/Easy/231.java
@@ -0,0 +1,18 @@
+//231. Power of Two
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 0) {
+            return false;
+        }
+
+        while(n != 1) {
+            if(n%2 != 0) {
+                return false;
+            }
+            n = n/2;
+        }
+
+        return true;
+    }
+}
