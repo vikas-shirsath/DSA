aa8c855ffe5c3beac26a37b3d4ecb08f5bd75403
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-10-03 22:28:49 +0530

    Practice

---
+++ LeetCode/Easy/217.java
@@ -0,0 +1,17 @@
+//217. Contains Duplicate
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+
+            seen.add(num);
+        }
+
+        return false;
+    }
+}
