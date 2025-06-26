28b5db46e1d3a3eddacb47b865d1bd74f0cf143d
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2025-06-26 21:56:54 +0530

    reverse array

---
+++ GFG/Array/reverseArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int left = 0, right = arr.length - 1;
+        while(left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
