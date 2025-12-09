5af4bf1ade78f9c10715f3277ef9420fff094f44
vikas-shirsath <vikasshirsath04@gmail.com>
2025-12-09 18:34:41 +0530

    remove Duplicate from sorted array - two pointers

---
+++ LeetCode/Medium/80.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int e = 0; e < nums.length; e++) {
+            if(i == 0 || i == 1 || nums[i-2] != nums[e]) {
+                nums[i] = nums[e];
+                i++;
+            }
+        }
+        return i;
+    }
+}
